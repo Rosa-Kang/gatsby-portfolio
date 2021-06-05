11179e91bf13c03ebe63c4dbc8faa992d37ff0b1
Rosa Kang <therosessom@gmail.com>
2021-06-04 21:47:34 -0700

    update thumbnail

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,7 +6,7 @@ module.exports = {
     description:
       'Rosa Kang is a software engineer who specializes in building exceptional digital experiences.',
     siteUrl: 'https://rosa-kang.netlify.app/', // No trailing slash allowed!
-    image: '/log.png', // Path to your image you placed in the 'static' folder
+    image: '/thumbnail.png', // Path to your image you placed in the 'static' folder
     twitterUsername: ' ',
   },
   plugins: [
